50e817d158bd03cd7ba5cbd9c5d57dafb6b2956a
khq1 <74719753+khq1@users.noreply.github.com>
2021-10-10 14:59:58 +0200

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Portfolio'`, () => {
+  it(`should have as title 'PortfolioApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('PortfolioApp');
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Portfolio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PortfolioApp app is running!');
   });
 });
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { CounterDataComponent } from '../components/counter-data/counter-data.component';
+
 
 @Component({
   selector: 'app-menu',
